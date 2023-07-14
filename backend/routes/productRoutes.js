02c6dadeb352e684c44c6c2bf131a397555284c6
GoncaloSantosdev <goncalosantosprofissional@gmail.com>
2023-07-14 14:35:09 +0100

    delete product functionality

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,11 +6,16 @@ import {
   getProducts,
   createProduct,
   updateProduct,
+  deleteProduct,
 } from "../controllers/productController.js";
 // Middleware
 import { protect, admin } from "../middlewares/authMiddleware.js";
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
-router.route("/:id").get(getProductById).put(protect, admin, updateProduct);
+router
+  .route("/:id")
+  .get(getProductById)
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
 
 export default router;
